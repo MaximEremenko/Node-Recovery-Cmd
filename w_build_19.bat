76a2e9066ded7d302d5ad7569607817d05ff3984
M.Eremenko <m.eremenko@skoltech.ru>
2021-06-02 19:15:31 +0300

    build script fixed

--- w_build_19.bat
+++ w_build_19.bat
@@ -3,5 +3,5 @@ rmdir /S /Q %BUILD_DIR%
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 cmake ../ -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=RelWithDebInfo
-cmake --build . --config Release --target package 
+cmake --build . --config Release
 cd ..
